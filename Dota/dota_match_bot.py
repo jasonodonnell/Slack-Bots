242276d84df46fd9fe4a3b1e5934a63fcf507950
Alex Mayer <amayer5125@gmail.com>
2016-12-08 11:32:26 -0500

    Normalize Spacing And Quotes

--- Dota/dota_match_bot.py
+++ Dota/dota_match_bot.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This bot is for getting live dota games currently being played.  
+# This bot is for getting live dota games currently being played.
 # Since trackdota tracks all games, we are only interested in those in
 # the whitelist file.  When a game is live, it sends a message to our
 # Slack chat room #dota
@@ -7,11 +7,11 @@ from bs4 import BeautifulSoup
 import urllib2
 import subprocess
 
-chat_url=''
-whitelist_dir='./team_whitelist.txt'
-trackdota_links_dir='./trackdota_links.txt'
+chat_url = ''
+whitelist_dir = './team_whitelist.txt'
+trackdota_links_dir = './trackdota_links.txt'
 
-with open(whitelist_dir)as whitelist_file:
+with open(whitelist_dir) as whitelist_file:
     whitelist = whitelist_file.read().splitlines()
 
 with open(trackdota_links_dir) as trackdota_file:
@@ -51,19 +51,19 @@ for i in range(0, len(links)):
     if radiant[i] in whitelist or dire[i] in whitelist:
         if links[i] not in trackdota_links:
             # Construct payload
-            radiant_team = radiant[i].replace("'","")
-            radiant_team = radiant_team.replace("`","")
-            dire_team= dire[i].replace("'","")
-            dire_team = dire_team.replace("`","")
-            match_string = "%s VS %s :: <%s|Click here> for TrackDota." % (radiant_team, dire_team, links[i])
-            curl_message='payload={"channel": "#dota", "username": "brunobot", "text": "%s", "icon_emoji": ":brunoface:"}' % (match_string)
-            
+            radiant_team = radiant[i].replace('\'', '')
+            radiant_team = radiant_team.replace('`', '')
+            dire_team= dire[i].replace('\'', '')
+            dire_team = dire_team.replace('`', '')
+            match_string = '%s VS %s :: <%s|Click here> for TrackDota.' % (radiant_team, dire_team, links[i])
+            curl_message = 'payload={"channel": "#dota", "username": "brunobot", "text": "%s", "icon_emoji": ":brunoface:"}' % (match_string)
+
             # Write trackdota url to file to check for later
             f = open(trackdota_links_dir, 'a')
             link = links[i] + '\n'
             f.write(link)
             f.close()
-            
-            # Execute curl 
-            curl_command="curl -X POST --data-urlencode '%s' %s" % (curl_message, chat_url)
-            subprocess.call("%s" % curl_command, shell=True)
+
+            # Execute curl
+            curl_command = 'curl -X POST --data-urlencode \'%s\' %s' % (curl_message, chat_url)
+            subprocess.call('%s' % curl_command, shell=True)
